ff578dae40f0d28b96b16172f831b0b1c8548038
Cristian <crgaonas24@gmail.com>
2020-10-27 14:29:46 -0500

    Agregando commits de forma manual

--- src/main/java/com/crisda24/kafka/consumers/Crisda24Consumer.java
+++ src/main/java/com/crisda24/kafka/consumers/Crisda24Consumer.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -23,15 +24,17 @@ public class Crisda24Consumer {
 		props.setProperty("auto.commit.interval.ms","1000");
 		props.setProperty("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
 		props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 		try(KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)){
-			//consumer.subscribe(Arrays.asList("crisda24-topic"));
-			TopicPartition topicPartition = new TopicPartition("crisda24-topic", 1);
+			consumer.subscribe(Arrays.asList("crisda24-topic"));
+			/*TopicPartition topicPartition = new TopicPartition("crisda24-topic", 1);
 			consumer.assign(Arrays.asList(topicPartition));
-			consumer.seek(topicPartition, 50);
+			consumer.seek(topicPartition, 50);*/
 			while(true) {
 				ConsumerRecords<String, String> consumerRecords=consumer.poll(Duration.ofMillis(100));
 				for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
 					log.info("Offset = {}, Partition {}, Key={}, Value{} ", consumerRecord.offset(), consumerRecord.partition(), consumerRecord.key(), consumerRecord.value());
+				    consumer.commitSync();
 				}
 			}
 			
